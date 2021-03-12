d23b5579aa64d00be1c2c9ddc3a901b03679d27a
Evgeny Prikazchikov <eprikazchikov@mail.ru>
2021-03-12 15:17:19 +0300

    Engine: Tiled option for the SpriteRender #324

--- engine/src/components/spriterender.cpp
+++ engine/src/components/spriterender.cpp
@@ -26,7 +26,8 @@ public:
             m_pCustomMesh(nullptr),
             m_Color(1.0f),
             m_Size(1.0f),
-            m_Hash(0) {
+            m_Hash(0),
+            m_DrawMode(false) {
 
     }
 
@@ -45,7 +46,6 @@ public:
 
     void composeMesh() {
         if(m_pSprite) {
-
             Mesh *mesh = m_pSprite->mesh(m_Hash);
             if(mesh) {
                 if(m_pCustomMesh == nullptr) {
@@ -56,33 +56,87 @@ public:
                 m_pCustomMesh->setLod(0, lod);
                 lod = m_pCustomMesh->lod(0);
                 Vector3Vector &vetrs = lod->vertices();
-                {
-                    float bl = vetrs[0].x;
-                    float br = vetrs[3].x;
-                    vetrs[ 0].x *= m_Size.x; vetrs[ 3].x *= m_Size.x;
-                    vetrs[ 4].x *= m_Size.x; vetrs[ 7].x *= m_Size.x;
-                    vetrs[ 8].x *= m_Size.x; vetrs[11].x *= m_Size.x;
-                    vetrs[12].x *= m_Size.x; vetrs[15].x *= m_Size.x;
-                    float dl = vetrs[0].x - bl;
-                    float dr = vetrs[3].x - br;
-                    vetrs[ 1].x += dl; vetrs[ 2].x += dr;
-                    vetrs[ 5].x += dl; vetrs[ 6].x += dr;
-                    vetrs[ 9].x += dl; vetrs[10].x += dr;
-                    vetrs[13].x += dl; vetrs[14].x += dr;
-                }
-                {
-                    float bl = vetrs[ 0].y;
-                    float br = vetrs[12].y;
-                    vetrs[ 0].y *= m_Size.y; vetrs[12].y *= m_Size.y;
-                    vetrs[ 1].y *= m_Size.y; vetrs[13].y *= m_Size.y;
-                    vetrs[ 2].y *= m_Size.y; vetrs[14].y *= m_Size.y;
-                    vetrs[ 3].y *= m_Size.y; vetrs[15].y *= m_Size.y;
-                    float dl = vetrs[ 0].y - bl;
-                    float dr = vetrs[12].y - br;
-                    vetrs[ 4].y += dl; vetrs[ 8].y += dr;
-                    vetrs[ 5].y += dl; vetrs[ 9].y += dr;
-                    vetrs[ 6].y += dl; vetrs[10].y += dr;
-                    vetrs[ 7].y += dl; vetrs[11].y += dr;
+                if(m_DrawMode == SpriteRender::Tiled) {
+                    Vector2Vector &uvs = lod->uv0();
+                    IndexVector &indices = lod->indices();
+
+                    Vector2 ubl(uvs[0]);
+                    Vector2 utr(uvs[15]);
+
+                    Vector3 size(vetrs[15] - vetrs[0]);
+                    int width = std::ceilf(m_Size.x / size.x);
+                    int height = std::ceilf(m_Size.y / size.y);
+
+                    vetrs.resize(width * height * 4);
+                    uvs.resize(width * height * 4);
+                    indices.resize(width * height * 6);
+
+                    Vector3 bl(Vector3(m_Size, 0.0f) * -0.5f);
+
+                    int i = 0;
+                    for(int y = 0; y < height; y++) {
+                        for(int x = 0; x < width; x++) {
+                            int index = (y * width + x) * 4;
+
+                            float factor = 1.0f;
+                            if(x == width - 1) {
+                                float value = (m_Size.x * 0.5 - bl.x) / size.x;
+                                factor = MIN(value, 1.0f);
+                            }
+                            Vector2 f(factor, 1.0f);
+
+                            vetrs[index] = bl;
+                            vetrs[index + 1] = bl + Vector3(size.x * f.x, 0.0f, 0.0f);
+                            vetrs[index + 2] = bl + Vector3(size.x * f.x, size.y * f.y, 0.0f);
+                            vetrs[index + 3] = bl + Vector3(0.0f, size.y * f.y, 0.0f);
+
+                            uvs[index] = ubl;
+                            uvs[index + 1] = ubl + Vector2((utr.x - ubl.x) * f.x, 0.0f);
+                            uvs[index + 2] = ubl + Vector2((utr.x - ubl.x) * f.x, (utr.y - ubl.y) * f.y);
+                            uvs[index + 3] = ubl + Vector2(0.0f, (utr.y - ubl.y) * f.y);
+
+                            indices[i]     = index;
+                            indices[i + 1] = index + 1;
+                            indices[i + 2] = index + 2;
+                            indices[i + 3] = index;
+                            indices[i + 4] = index + 2;
+                            indices[i + 5] = index + 3;
+
+                            bl.x += size.x;
+
+                            i += 6;
+                        }
+                        bl.y += size.y;
+                    }
+                } else {
+                    {
+                        float bl = vetrs[0].x;
+                        float br = vetrs[3].x;
+                        vetrs[ 0].x *= m_Size.x; vetrs[ 3].x *= m_Size.x;
+                        vetrs[ 4].x *= m_Size.x; vetrs[ 7].x *= m_Size.x;
+                        vetrs[ 8].x *= m_Size.x; vetrs[11].x *= m_Size.x;
+                        vetrs[12].x *= m_Size.x; vetrs[15].x *= m_Size.x;
+                        float dl = vetrs[0].x - bl;
+                        float dr = vetrs[3].x - br;
+                        vetrs[ 1].x += dl; vetrs[ 2].x += dr;
+                        vetrs[ 5].x += dl; vetrs[ 6].x += dr;
+                        vetrs[ 9].x += dl; vetrs[10].x += dr;
+                        vetrs[13].x += dl; vetrs[14].x += dr;
+                    }
+                    {
+                        float bl = vetrs[ 0].y;
+                        float br = vetrs[12].y;
+                        vetrs[ 0].y *= m_Size.y; vetrs[12].y *= m_Size.y;
+                        vetrs[ 1].y *= m_Size.y; vetrs[13].y *= m_Size.y;
+                        vetrs[ 2].y *= m_Size.y; vetrs[14].y *= m_Size.y;
+                        vetrs[ 3].y *= m_Size.y; vetrs[15].y *= m_Size.y;
+                        float dl = vetrs[ 0].y - bl;
+                        float dr = vetrs[12].y - br;
+                        vetrs[ 4].y += dl; vetrs[ 8].y += dr;
+                        vetrs[ 5].y += dl; vetrs[ 9].y += dr;
+                        vetrs[ 6].y += dl; vetrs[10].y += dr;
+                        vetrs[ 7].y += dl; vetrs[11].y += dr;
+                    }
                 }
 
                 m_pCustomMesh->setFlags(mesh->flags());
@@ -110,6 +164,8 @@ public:
 
     string m_Item;
     int m_Hash;
+
+    int m_DrawMode;
 };
 /*!
     \class SpriteRender
@@ -117,6 +173,13 @@ public:
     \inmodule Engine
 
     The SpriteRender component allows you to display images as sprites to use in both 2D and 3D scenes.
+*/
+
+/*!
+    \enum SpriteRender::DrawMode
+
+    \value Sliced \c The SpriteRender will respect 9 patch slicing rules for the size property.
+    \value Tiled \c The SpriteRender will duplicate sprites to fill the size property rectangle.
 */
 
 SpriteRender::SpriteRender() :
@@ -264,6 +327,21 @@ Vector2 SpriteRender::size() const {
 */
 void SpriteRender::setSize(const Vector2 &size) {
     p_ptr->m_Size = size;
+    p_ptr->composeMesh();
+}
+/*!
+    Returns a draw mode for the sprite.
+    Please check SpriteRender::DrawMode for more details.
+*/
+int SpriteRender::drawMode() const {
+    return p_ptr->m_DrawMode;
+}
+/*!
+    Sets a draw \a mode for the sprite.
+    Please check SpriteRender::DrawMode for more details.
+*/
+void SpriteRender::setDrawMode(int mode) {
+    p_ptr->m_DrawMode = mode;
     p_ptr->composeMesh();
 }
 /*!
