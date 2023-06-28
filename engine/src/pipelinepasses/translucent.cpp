1e80ad9640aa476193a1a7922e0995e9aa736b08
Evgeny Prikazchikov <eprikazchikov@mail.ru>
2023-06-28 07:00:53 +0300

    Render: Translucent materials ignores depth test #535

---
+++ engine/src/pipelinepasses/translucent.cpp
@@ -0,0 +1,38 @@
+#include "pipelinepasses/translucent.h"
+
+#include "components/actor.h"
+#include "components/transform.h"
+
+#include "resources/rendertarget.h"
+#include "resources/mesh.h"
+
+#include "pipelinecontext.h"
+#include "commandbuffer.h"
+
+Translucent::Translucent() :
+    m_translucentPass(Engine::objectCreate<RenderTarget>("translucentPass")) {
+
+}
+
+Texture *Translucent::draw(Texture *source, PipelineContext *context) {
+    CommandBuffer *buffer = context->buffer();
+
+    buffer->setRenderTarget(m_translucentPass);
+
+    // Transparent pass
+    context->drawRenderers(CommandBuffer::TRANSLUCENT, context->culledComponents());
+
+    return source;
+}
+
+void Translucent::setInput(uint32_t index, Texture *texture) {
+    switch(index) {
+    case Emissve: m_translucentPass->setColorAttachment(Emissve, texture); break;
+    case Depth: m_translucentPass->setDepthAttachment(texture); break;
+    default: break;
+    }
+}
+
+uint32_t Translucent::layer() const {
+    return CommandBuffer::TRANSLUCENT;
+}
