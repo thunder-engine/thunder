65be39eae4ee70432ff5edce26cdfbf505325fd7
Evgeniy Prikazchikov <eprikazchikov@mail.ru>
2024-12-26 08:19:02 +0300

    Editor: Name of Actor disappeared from Properties #896 (#897)

--- engine/includes/components/actor.h
+++ engine/includes/components/actor.h
@@ -14,7 +14,8 @@ class ENGINE_EXPORT Actor : public Object {
 
     A_PROPERTIES(
         A_PROPERTY(bool, enabled, Actor::isEnabled, Actor::setEnabled),
-        A_PROPERTY(bool, static, Actor::isStatic, Actor::setStatic)
+        A_PROPERTY(bool, static, Actor::isStatic, Actor::setStatic),
+        A_PROPERTY(string, name, Actor::name, Actor::setName)
     )
 
     A_METHODS(
