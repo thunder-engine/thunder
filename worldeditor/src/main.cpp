dfba321e13e57f087014739775559ec0c8f75a09
Evgeny Prikazchikov <eprikazchikov@mail.ru>
2025-02-01 20:21:02 +0300

    update

--- worldeditor/src/main.cpp
+++ worldeditor/src/main.cpp
@@ -35,8 +35,6 @@ int main(int argc, char *argv[]) {
     QCoreApplication::setApplicationName(EDITOR_NAME);
     QCoreApplication::setApplicationVersion(SDK_VERSION);
 
-    qputenv(qPrintable(gRhi), "RenderVK");
-
     QFile qss(":/Style/styles/dark/style.qss");
     if(qss.open(QIODevice::ReadOnly)) {
         qApp->setStyleSheet(qss.readAll());
