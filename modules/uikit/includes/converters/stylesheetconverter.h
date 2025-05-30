c2629b83c1782b2a01760f3d2266128d1ea85f1f
Evgeny Prikazchikov <eprikazchikov@mail.ru>
2025-05-30 10:06:03 +0300

    Editor: Add Document and Style Sheet asset types. #720

--- modules/uikit/includes/converters/stylesheetconverter.h
+++ modules/uikit/includes/converters/stylesheetconverter.h
@@ -2,7 +2,6 @@
 #define STYLESHEETCONVERTER_H
 
 #include <editor/assetconverter.h>
-#include <resources/stylesheet.h>
 
 class StyleSheetConverterSettings : public AssetConverterSettings {
 public:
@@ -17,6 +16,8 @@ class StyleSheetConverter : public AssetConverter {
     QStringList suffixes() const override { return {"css"}; }
     ReturnCode convertFile(AssetConverterSettings *s) override;
     AssetConverterSettings *createSettings() override;
+
+    QString templatePath() const override;
 };
 
 #endif // STYLESHEETCONVERTER_H
