36218a1ca75b25b75e618aa89ea22b2cb697614c
Evgeny Prikazchikov <eprikazchikov@mail.ru>
2025-10-08 22:21:54 +0300

    update

--- engine/src/components/actor.cpp
+++ engine/src/components/actor.cpp
@@ -513,7 +513,7 @@ void Actor::loadUserData(const VariantMap &data) {
 */
 Variant Actor::loadObject(Variant &value) {
     if(value.type() == MetaType::STRING) { // Asset
-        Resource *res = Engine::loadResource(value.toString());
+        Object *res = Engine::loadResource(value.toString());
         if(res) {
             const char *name = res->metaObject()->name();
             return Variant(MetaType::type(name)+1, &res);
