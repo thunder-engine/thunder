6a80427e797f5c47d52d5de5a73aa69d05fe60b5
Evgeny Prikazchikov <eprikazchikov@mail.ru>
2026-09-11 09:08:29 +0300

    Editor: Project rebuilds each time editor runs #1400

--- modules/editor/buildtools/converter/vsbuilder.cpp
+++ modules/editor/buildtools/converter/vsbuilder.cpp
@@ -6,10 +6,12 @@
 #include <file.h>
 
 #include <editor/projectsettings.h>
+#include <editor/editorsettings.h>
 #include <os/processenvironment.h>
 
 namespace {
     const char *gEditorSuffix("-editor");
+    const char *gMsBuildPath("Builder/VisualStudio/MSBuildPath");
 
 #ifndef _DEBUG
     const char *gMode("Release");
@@ -23,20 +25,9 @@ VsBuilder::VsBuilder() {
 
     connect(&m_process, _SIGNAL(finished(int)), this, _SLOT(onBuildFinished(int)));
 
-    TString vswherePath("C:/Program Files (x86)/Microsoft Visual Studio/Installer/vswhere.exe");
-
-    if(File::exists(vswherePath)) {
-        Process vswhere;
-        if(vswhere.start(vswherePath, {"-latest", "-property", "installationPath"})) {
-            if(vswhere.waitForStarted() && vswhere.waitForFinished()) {
-                m_vsPath = vswhere.readAllStandardOutput().trimmed();
-                if(!m_vsPath.isEmpty()) {
-                    m_vsPath.replace('\\', '/');
-                    m_vsPath += "/Msbuild/Current/Bin/MSBuild.exe";
-                }
-            }
-        }
-    }
+    EditorSettings *settings = Editor::settings();
+    settings->registerValue(gMsBuildPath, "", "editor=Path");
+    m_vsPath = settings->value(gMsBuildPath).toString();
 
     m_filePref = "    <ClCompile Include=\""; m_fileSuff = "\" />"; m_fileSep = "";
     m_incPathSep = ";";
@@ -65,6 +56,29 @@ VsBuilder::VsBuilder() {
 
 bool VsBuilder::buildProject() {
     if(m_outdated && !m_process.isRunning()) {
+        m_vsPath = Editor::settings()->value(gMsBuildPath).toString();
+        if(m_vsPath.isEmpty() || !File::exists(m_vsPath)) {
+            TString vswherePath("C:/Program Files (x86)/Microsoft Visual Studio/Installer/vswhere.exe");
+            if(File::exists(vswherePath)) {
+                Process vswhere;
+                TString vswhereProgram = TString("\"") + vswherePath + "\"";
+                if(vswhere.start(vswhereProgram, {"-latest", "-property", "installationPath"}) &&
+                   vswhere.waitForStarted() && vswhere.waitForFinished()) {
+                    TString installationPath = vswhere.readAllStandardOutput().trimmed();
+                    if(!installationPath.isEmpty()) {
+                        installationPath.replace('\\', '/');
+                        m_vsPath = installationPath + "/Msbuild/Current/Bin/MSBuild.exe";
+                        Editor::settings()->setValue(gMsBuildPath, m_vsPath);
+                    }
+                }
+            }
+        }
+
+        if(m_vsPath.isEmpty() || !File::exists(m_vsPath)) {
+            aError() << name() << "Unable to find MSBuild.exe at:" << m_vsPath;
+            return false;
+        }
+
         generateProject();
 
         ProjectSettings *mgr = Editor::project();
